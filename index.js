8453130cf02dc60463a63c31f47b2c475eb8e394
Je Yeong Soh <sohjeyeong@gmail.com>
2023-07-06 23:06:58 -0700

    Added CORS handling, allowing all origins.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
 const port = 3100
+
+app.use(cors({ origin: '*' }))
 
 app.get('/', (req, res) => res.send('Welcome!'))
 
