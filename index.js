08402f96ef1f064795bbac25612991ac839ca98d
Je Yeong Soh <sohjeyeong@gmail.com>
2023-07-09 23:53:29 -0700

    Able to read email content.

--- index.js
+++ index.js
@@ -88,17 +88,51 @@ const subscriptionNameOrId =
 
 const pubSubClient = new PubSub()
 
+const getMessage = async (email, messageId) => {
+  try {
+    const url = `https://gmail.googleapis.com/gmail/v1/users/${email}/messages/${messageId}`
+    const { token } = await oAuth2Client.getAccessToken()
+    const config = generateConfig(url, token)
+    const response = await axios(config)
+
+    const messageInPlaintextBase64 =
+      response?.data?.payload?.parts?.[0]?.body?.data
+
+    if (messageInPlaintextBase64) {
+      const messageInPlaintextUTF8 = Buffer.from(
+        messageInPlaintextBase64,
+        'base64'
+      ).toString('utf-8')
+
+      console.log(`The message in the email is:\n${messageInPlaintextUTF8}`)
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
+
 const getHistory = async (email, historyId) => {
   try {
     const url = `https://gmail.googleapis.com/gmail/v1/users/${email}/history?startHistoryId=${historyId}&labelId=${'INBOX'}`
     const { token } = await oAuth2Client.getAccessToken()
     const config = generateConfig(url, token)
     const response = await axios(config)
-    console.log(response.data.history)
+
+    const emails = response.data?.history
+    const firstEmail = emails?.[0]
+    const firstEmailId = firstEmail?.messages?.[0]?.id
+
+    if (firstEmailId) {
+      await getMessage(email, firstEmailId)
+    } else {
+      console.log('No email to read.')
+    }
   } catch (error) {
     console.error(error)
   }
 }
+
+let previousHistoryID = '1927186'
 
 function listenForEmails() {
   // References an existing subscription
@@ -106,10 +140,13 @@ function listenForEmails() {
 
   // Create an event handler to handle messages
   const messageHandler = async (message) => {
-    const { emailAddress, historyId } = message.data
+    const { emailAddress, historyId } = JSON.parse(message.data.toString())
 
     if (emailAddress === 'sohjeyeong@gmail.com' && historyId) {
-      await getHistory(emailAddress, historyId)
+      console.log(`Message received. History ID is ${historyId}`)
+      await getHistory(emailAddress, previousHistoryID)
+
+      previousHistoryID = historyId
     }
 
     // "Ack" (acknowledge receipt of) the message
@@ -118,6 +155,8 @@ function listenForEmails() {
 
   // Listen for new messages until timeout is hit
   subscription.on('message', messageHandler)
+
+  console.log(`Listening for Pub/sub: ${subscriptionNameOrId}`)
 }
 
 listenForEmails()
