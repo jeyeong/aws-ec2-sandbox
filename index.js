1035d002c0ba845244f517e78aaddb1db2268bfc
Je Yeong Soh <sohjeyeong@gmail.com>
2023-07-10 23:08:22 -0700

    Testing out Firebase on AWS.

--- index.js
+++ index.js
@@ -13,6 +13,31 @@ const { listenForEmails } = require('./handlers/gmailPubSubListener')
 
 listenForEmails()
 
+// Firebase.
+const admin = require('firebase-admin')
+const serviceAccount = require('./autoinvoice-a1471-firebase-adminsdk-ugjgo-2c27f80183.json')
+const {
+  getFirestore,
+  Timestamp,
+  FieldValue,
+  Filter,
+} = require('firebase-admin/firestore')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://autoinvoice-a1471-default-rtdb.firebaseio.com',
+})
+
+const db = getFirestore()
+
+db.collection('users')
+  .get()
+  .then((snapshot) => {
+    snapshot.forEach((doc) => {
+      console.log(doc.id, '=>', doc.data())
+    })
+  })
+
 // Express app.
 const app = express()
 const port = 3100
